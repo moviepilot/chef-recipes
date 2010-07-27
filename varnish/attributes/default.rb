24a449b99ee748e0e157ae4c195abcea3be1130d
Caspar Clemens Mierau <ccm@screenage.de>
2010-07-27 14:24:56 +0200

    changed package tar.gz naming scheme to generic

--- varnish/attributes/default.rb
+++ varnish/attributes/default.rb
@@ -1,6 +1,5 @@
 default[:varnish] = {}
-default[:varnish][:version] = '0.9.9'
+default[:varnish][:version] = '2.1.2'
 default[:varnish][:package_name] = "varnish-#{node[:varnish][:version]}.tar.gz"
 default[:varnish][:memory_limit] = "256M"
-default[:varnish][:cron_interval] = 10
 default[:varnish][:port] = 9312
--- varnish/recipes/install.rb
+++ varnish/recipes/install.rb
@@ -8,20 +8,17 @@ directory "/tmp/varnish_install" do
 end
 
 remote_file "/tmp/varnish_install/#{node[:varnish][:package_name]}" do
-#  source "http://www.varnishsearch.com/downloads/#{node[:varnish][:package_name]}"
-  source "http://downloads.sourceforge.net/project/varnish/varnish/2.1.2/varnish-2.1.2.tar.gz?use_mirror=ovh&ts=1280224361"
+  source "http://downloads.sourceforge.net/project/varnish/varnish/#{node[:varnish][:version]}/varnish-#{node[:varnish][:version]}.tar.gz?use_mirror=ovh&ts=1280224361"
   mode "0644"
   action :create_if_missing
 end
 
 execute "untar varnish archive" do
- # command "tar xvfz #{node[:varnish][:package_name]}"
- command "tar xvfz varnish-2.1.2.tar.gz"
+ command "tar xvfz #{node[:varnish][:package_name]}"
   cwd "/tmp/varnish_install"
 end
 
 execute "./configure --with-mysql && make && make install" do
-#  cwd "/tmp/varnish_install/varnish-#{node[:varnish][:version]}"
-  cwd "/tmp/varnish_install/varnish-2.1.2"
+  cwd "/tmp/varnish_install/varnish-#{node[:varnish][:version]}"
 end
 
