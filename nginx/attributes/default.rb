47cb1cc7fba51675acd5073fd125d1125c9e20fd
Caspar Clemens Mierau <ccm@screenage.de>
2010-07-27 11:04:43 +0200

    removed nginx

--- nginx/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default[:nginx] = {}
-default[:nginx][:version] = '0.8.7'
-default[:nginx][:package_name] = "nginx-#{node[:nginx][:version]}.tar.gz"
-default[:nginx][:memory_limit] = "256M"
-default[:nginx][:cron_interval] = 10
-default[:nginx][:port] = 9312
