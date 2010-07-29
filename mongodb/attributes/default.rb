bfda10150e930e1a03902220b3919f948c9ac563
Benjamin Krause <bk@benjaminkrause.com>
2010-07-29 23:57:23 +0200

    more monigodb tweaking, using pidof to fetch mongod pid

--- mongodb/attributes/default.rb
+++ mongodb/attributes/default.rb
@@ -1,6 +1,8 @@
 default[:mongodb] = {}
-default[:mongodb][:version] = '1.4.4'
-default[:mongodb][:arch] = `uname -m`.strip
-default[:mongodb][:dir_name] = "mongodb-linux-#{node[:mongodb][:arch]}-#{node[:mongodb][:version]}"
+default[:mongodb][:version]      = '1.4.4'
+default[:mongodb][:arch]         = `uname -m`.strip
+default[:mongodb][:dir_name]     = "mongodb-linux-#{node[:mongodb][:arch]}-#{node[:mongodb][:version]}"
 default[:mongodb][:package_name] = "mongodb-linux-#{node[:mongodb][:arch]}-#{node[:mongodb][:version]}.tgz"
-default[:mongodb][:port] = "27017"
+default[:mongodb][:db_directory] = '/var/lib/mongodb'
+default[:mongodb][:pidfile]      = '/var/run/mongod.pid'
+default[:mongodb][:port]         = "27017"
--- mongodb/recipes/install.rb
+++ mongodb/recipes/install.rb
@@ -7,11 +7,11 @@ directory "/tmp/mongodb_install" do
   action :create
 end
 
-directory "/var/mongodb" do
+directory "#{node[:mongodb][:db_directory]}" do
   mode "0750"
-  action :create_if_missing
+  action :create
+  not_if "test -d #{node[:mongodb][:db_directory]}"
 end
-
 
 remote_file "/tmp/mongodb_install/#{node[:mongodb][:package_name]}" do
   source "http://fastdl.mongodb.org/linux/#{node[:mongodb][:package_name]}"
@@ -30,8 +30,7 @@ execute "mv /tmp/mongodb_install/#{node[:mongodb][:dir_name]} /opt/#{node[:mongo
 end
 
 execute "rm /opt/mongodb" do
-  action :delete
-  only_if "test -L /opt/#{node[:mongodb][:dir_name]}"
+  only_if "test -L /opt/mongodb"
 end
 
 execute "ln -s /opt/#{node[:mongodb][:dir_name]} /opt/mongodb" do
@@ -45,10 +44,15 @@ template "/etc/init.d/mongod" do
   mode "0755"
 end
 
+template "/etc/monit/conf.d/mongodb.monitrc" do
+  source "mongo.monitrc.erb"
+  owner "root"
+  group "root"
+  mode  "0644"
+end
+
 execute "update-rc.d mongod defaults" do
   cwd "/"
 end
 
-execute "/etc/init.d/mongod start" do
-  cwd "/"
-end
+execute "monit monitor mongodb"
