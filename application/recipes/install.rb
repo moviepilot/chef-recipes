31b159a83681c0a22ff6827af7cc14b9bcab1b39
Benjamin Krause <bk@benjaminkrause.com>
2010-07-30 13:50:52 +0200

    first shot at our own deployment recipes

---
+++ application/recipes/install.rb
@@ -0,0 +1,16 @@
+include_recipe "deploy::user"
+
+node[:deploy].each do |application, deploy|
+  # create shared/ directory structure
+  %w(log config system pids bundle).each do |dir_name|
+    directory "#{deploy[:deploy_to]}/shared/#{dir_name}" do
+      group deploy[:group]
+      owner deploy[:user]
+      mode "0770"
+      action :create
+      recursive true
+    end
+  end
+end
+
+include_recipe "deploy::source"
--- nginx-appserver/templates/default/nginx.monitrc.rb
+++
@@ -1,4 +0,0 @@
-check process nginx with pidfile /var/run/nginx.pid
-  start program = "/etc/init.d/nginx start"
-  stop  program = "/etc/init.d/nginx stop"
-  if failed host 127.0.0.1 port 8080 then restart
