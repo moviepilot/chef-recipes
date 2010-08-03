a41c963af9d99ceb34d32efee9e0b2fcab5ed9bb
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-03 15:26:15 +0200

    initial scalper recipe

---
+++ scalper/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Moviepilot GmbH"
+maintainer_email  "developers@moviepilot.com"
+license           "Apache 2.0"
+description       "Installs Scalarium Helper 'scalper'"
+version           "0.1"
+recipe            "scalper::install", "Installs scalper and json gem dependency"
+
+supports 'ubuntu'
---
+++ scalper/recipes/install.rb
@@ -0,0 +1,5 @@
+execute "bash < <(curl http://github.com/moviepilot/scalper/raw/master/scalper-installer)" do
+  command "bash < <(curl http://github.com/moviepilot/scalper/raw/master/scalper-installer)"
+  action :run
+end
+
