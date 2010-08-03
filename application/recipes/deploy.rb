11bd9e30f523025ee6bad2d8e77732fc22ed298d
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-03 12:35:19 +0200

    changed gem install recipe

--- application/recipes/deploy.rb
+++ application/recipes/deploy.rb
@@ -1,11 +1,14 @@
 include_recipe "application::checkout"
 
 node[:deploy].each do |application, deploy|
-  Dir["#{deploy[:current_path]}/vendor/gems/*.gem"].each do |gem_file|
-    # :TODO: Add a check if the gem is already installed
-    execute "install gem #{gem_file}" do
-      command "gem install #{gem_file} --no-ri --no-rdoc -q"
-    end
+  #Dir["#{deploy[:current_path]}/vendor/gems/*.gem"].each do |gem_file|
+  #  # :TODO: Add a check if the gem is already installed
+  #  execute "install gem #{gem_file}" do
+  #    command "gem install #{gem_file} --no-ri --no-rdoc -q"
+  #  end
+  #end
+  execute "gem install #{deploy[:current_path]}/vendor/gems/*.gem" do
+	  command "gem install #{deploy[:current_path]}/vendor/gems/*.gem"
   end
   execute "bundle" do
     cwd deploy[:release_path]
