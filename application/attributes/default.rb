b0a83b2a064aa11f47aab112d0062d621b519fee
Daniel Bornkessel <github@bornkessel.com>
2010-08-05 12:42:56 +0200

    configure nginx

--- application/attributes/default.rb
+++ application/attributes/default.rb
@@ -9,7 +9,7 @@ deploy.each do |application, deploy|
   default[:deploy][application][:release]         = Time.now.utc.strftime("%Y%m%d%H%M%S")
   default[:deploy][application][:release_path]    = "#{deploy[:deploy_to]}/releases/#{deploy[:release]}"
   default[:deploy][application][:current_path]    = "#{deploy[:deploy_to]}/current"
-  default[:deploy][application][:document_root]   = "#{deploy[:deploy_to]}/current/public"
+  default[:deploy][application][:mp_document_root]   = "#{deploy[:deploy_to]}/current/public"
   default[:deploy][application][:rails_env]       = 'production'
   default[:deploy][application][:sleep_before_restart] = 0
 end
