265ce1ffa60dff13aa7e5dea7a48c11c475d260a
Daniel Bornkessel <github@bornkessel.com>
2010-08-05 18:17:28 +0200

    remove crap

--- application/recipes/deploy.rb
+++ application/recipes/deploy.rb
@@ -1,12 +1,6 @@
 include_recipe "application::checkout"
 
 node[:deploy].each do |application, deploy|
-  #Dir["#{deploy[:current_path]}/vendor/gems/*.gem"].each do |gem_file|
-  #  # :TODO: Add a check if the gem is already installed
-  #  execute "install gem #{gem_file}" do
-  #    command "gem install #{gem_file} --no-ri --no-rdoc -q"
-  #  end
-  #end
   execute "gem install #{deploy[:current_path]}/vendor/gems/*.gem --no-ri --no-rdoc" do
 	  command "gem install #{deploy[:current_path]}/vendor/gems/*.gem --no-ri --no-rdoc"
   end
@@ -14,10 +8,4 @@ node[:deploy].each do |application, deploy|
     cwd deploy[:release_path]
     command "cd #{deploy[:current_path]} && bundle install --without test:development"
   end
-
-  #template "#{node[:nginx_appserver][:vhost_dir]}/site-moviepilot" do
-  #  source "site-moviepilot.nginx.erb"
-  #  owner  node[:nginx_appserver][:user]
-  #  mode   0644
-  #end
 end
