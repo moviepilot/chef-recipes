ae7a5538d1a4718d262b227729b16902f1e1a501
Daniel Bornkessel <github@bornkessel.com>
2010-08-05 16:03:28 +0200

    fix scalper installer

--- scalper/recipes/install.rb
+++ scalper/recipes/install.rb
@@ -1,6 +1,10 @@
-execute "bash < <(curl http://github.com/moviepilot/scalper/raw/master/scalper-installer)" do
-  command "curl http://github.com/moviepilot/scalper/raw/master/scalper-installer > /tmp/scalper-installer"
-  command "sh /tmp/scalper-installer"
-  action :run
+execute "url http://github.com/moviepilot/scalper/raw/master/scalper-installer > /tmp/scalper-installer" do
+    command "curl http://github.com/moviepilot/scalper/raw/master/scalper-installer > /tmp/scalper-installer"
+    action :run
+end
+
+execute "sh /tmp/scalper-installer" do
+    command "sh /tmp/scalper-installer"
+    action :run
 end
 
