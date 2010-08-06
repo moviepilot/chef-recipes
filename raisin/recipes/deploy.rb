aae94c26840ca41525e325bfd75252bf3aaddfe1
Daniel Bornkessel <github@bornkessel.com>
2010-08-06 16:47:32 +0200

    raisin deploy script

--- raisin/recipes/deploy.rb
+++ raisin/recipes/deploy.rb
@@ -1,7 +1,12 @@
-include_recipe "application::checkout"
+include_recipe "raisin::checkout"
 
 node[:deploy].each do |application, deploy|
   #execute "create symbolic link to resque config" do
   #  command "ln -sf #{node[:resque][:resque_config]} #{deploy[:resque_config]}"
   #end
+  #
+  execute "build raisin" do
+    cwd deploy[:current_path]
+    command "./script/build.sh ."
+  end
 end
