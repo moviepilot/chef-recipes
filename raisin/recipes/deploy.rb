0ac3f9ea5470108d09d326fe269f7a522775509d
Daniel Bornkessel <github@bornkessel.com>
2010-08-06 18:39:34 +0200

    start raisin after deploy

--- raisin/recipes/deploy.rb
+++ raisin/recipes/deploy.rb
@@ -11,4 +11,18 @@ node[:deploy].each do |application, deploy|
     cwd deploy[:current_path]
     command "./script/build.sh #{deploy[:current_path]}"
   end
+
+  execute "create pid dir" do
+    command "mkdir -p #{deploy[:current_path]/pids"
+  end
+
+  execute "stop raisin if it's running" do
+    cwd deploy[:current_path]
+    command "./script/stop_raisin_manager.sh #{deploy[:current_path]} 80"
+  end
+
+  execute "start raisin" do
+    cwd deploy[:current_path]
+    command "./script/start_raisin_manager.sh #{deploy[:current_path]} 80"
+  end
 end
