124d16d90dc01d8489675596216d0d5695a029a6
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-09 15:05:41 +0200

    agreeing to whatever sun tells us

--- raisin/recipes/install.rb
+++ raisin/recipes/install.rb
@@ -1,5 +1,14 @@
 include_recipe "raisin::user"
 include_recipe "raisin::directory"
+
+execute "yes sun: we fully agree" do
+	command "echo 'sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true' | debconf-set-selections"
+end
+
+execute "aggreeing to java licencse" do
+	command "echo 'sun-java6-jdk shared/accepted-sun-dlj-v1-1 boolean true' | debconf-set-selections"
+end
+
 package "sun-java6-jdk"
 package "maven2"
 
