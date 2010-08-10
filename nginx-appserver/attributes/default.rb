a01c8982f9f95fcb2097a15370573943b390387c
Daniel Bornkessel <github@bornkessel.com>
2010-08-10 18:05:27 +0200

    correct passenger path

--- nginx-appserver/attributes/default.rb
+++ nginx-appserver/attributes/default.rb
@@ -1,6 +1,7 @@
 default[:nginx_appserver] = {}
 default[:nginx_appserver][:version]        = '2.2.15'
-default[:nginx_appserver][:passenger_root] = "#{`gem environment gemdir`.strip}/gems/passenger-#{default[:nginx_appserver][:version]}"
+#default[:nginx_appserver][:passenger_root] = "#{`gem environment gemdir`.strip}/gems/passenger-#{default[:nginx_appserver][:version]}"
+default[:nginx_appserver][:passenger_root] = "/usr/local/lib/ruby/gems/1.8/passenger-#{default[:nginx_appserver][:version]}"
 default[:nginx_appserver][:user]           = 'www-data'
 default[:nginx_appserver][:root_dir]       = '/opt/nginx'
 default[:nginx_appserver][:cache_dir]      = '/var/cache/nginx'
