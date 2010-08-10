3ab71096494cb630b0bca104e7c729b090d50e9f
Daniel Bornkessel <github@bornkessel.com>
2010-08-10 18:12:10 +0200

    restart app after raising configure

--- raisin/recipes/configure.rb
+++ raisin/recipes/configure.rb
@@ -2,11 +2,17 @@ require 'rubygems'
 require 'json'
 include_recipe "deploy" # get the deployment attributes
 
+
 node[:deploy].each do |application, deploy|
   execute "create config directory" do
     command "mkdir -p #{node[:raisin][:config_file].gsub(/\/[^\/]+$/, '')}"
   end
 
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command "touch tmp/restart.txt"
+    action :nothing
+  end
 
 
   cluster_state = JSON.load( File.read("/var/lib/scalarium/cluster_state.json") )
@@ -19,9 +25,9 @@ node[:deploy].each do |application, deploy|
     owner deploy[:user]
     variables :host => cluster_state["roles"]["raisin"]["instances"].first.last["private_dns_name"],
               :port => node[:raisin][:port]
-    
-    #notifies :run, resources(:execute => "restart Rails app #{application}")
-    
+
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+
     only_if do
       File.directory?("#{deploy[:deploy_to]}/current")
     end
