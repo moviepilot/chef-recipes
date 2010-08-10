d6e41cc7884a1525217f3c9e830adb8b6611992b
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-10 15:14:42 +0200

    massive update to memcached-upstream

--- memcached-upstream/recipes/install.rb
+++ memcached-upstream/recipes/install.rb
@@ -8,7 +8,7 @@ directory "/tmp/memcached-upstream_install" do
 end
 
 remote_file "/tmp/memcached-upstream_install/#{node[:memcached_upstream][:package_name]}" do
-  source "http://memcached.googlecode.com/files//#{node[:memcached_upstream][:upstream_url]}"
+  source "#{node[:memcached_upstream][:upstream_url]}"
   mode "0644"
   action :create_if_missing
 end
@@ -19,7 +19,7 @@ execute "untar memcached-upstream archive" do
 end
 
 execute "./configure --prefix=#{node[:memcached_upstream][:install_dir]} && make && make install" do
-  cwd "/tmp/memcached-upstream_install/memcached-upstream-#{node[:memcached_upstream][:version]}"
+  cwd "/tmp/memcached-upstream_install/memcached-#{node[:memcached_upstream][:version]}"
 end
 
 execute "rm /opt/memcached-upstream" do
@@ -30,4 +30,29 @@ execute "ln -s #{node[:memcached_upstream][:install_dir]} /opt/memcached-upstrea
   cwd "/opt"
 end
 
+directory "#{node[:memcached_upstream][:log_dir]}" do
+  owner node[:memcached_upstream][:user]
+  mode "0755"
+  action :create
+end
+
+template "/etc/init.d/memcached-upstream" do
+  source "init-script.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+template "/etc/monit/conf.d/memcached-upstream.monitrc" do
+  source "memcached-upstream.monitrc.erb"
+  owner "root"
+  group "root"
+  mode  "0644"
+end
+
+execute "update-rc.d memcached-upstream defaults" do
+  cwd "/"
+end
+
+execute "monit monitor memcached-upstream"
 
