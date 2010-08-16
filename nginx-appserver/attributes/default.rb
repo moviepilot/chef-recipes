f1b1d831027f2717c889a468302f15f97846482d
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-16 12:33:12 +0200

    cleaning up code

--- nginx-appserver/attributes/default.rb
+++ nginx-appserver/attributes/default.rb
@@ -1,7 +1,6 @@
 default[:nginx_appserver] = {}
 default[:nginx_appserver][:version]        = '2.2.15'
 default[:nginx_appserver][:ruby_path]      = '/opt/ruby'
-#default[:nginx_appserver][:passenger_root] = "#{`gem environment gemdir`.strip}/gems/passenger-#{default[:nginx_appserver][:version]}"
 default[:nginx_appserver][:passenger_root] = "#{`#{default[:nginx_appserver][:ruby_path]}/bin/gem environment gemdir`.strip}/gems/passenger-#{default[:nginx_appserver][:version]}"
 default[:nginx_appserver][:user]           = 'www-data'
 default[:nginx_appserver][:root_dir]       = '/opt/nginx'
@@ -13,4 +12,3 @@ default[:nginx_appserver][:pid_file]       = '/var/run/nginx.pid'
 default[:nginx_appserver][:log_dir]        = '/var/log/nginx'
 default[:nginx_appserver][:error_log]      = '/var/log/nginx/error.log'
 default[:nginx_appserver][:port]           = '80'
-#{`gem environment gemdir`.strip}
