4b267b7cfde49487a4046231d1646aba6d5601dd
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-16 12:09:09 +0200

    checking nested variables

--- nginx-appserver/attributes/default.rb
+++ nginx-appserver/attributes/default.rb
@@ -2,7 +2,7 @@ default[:nginx_appserver] = {}
 default[:nginx_appserver][:version]        = '2.2.15'
 default[:nginx_appserver][:ruby_path]      = '/opt/ruby'
 #default[:nginx_appserver][:passenger_root] = "#{`gem environment gemdir`.strip}/gems/passenger-#{default[:nginx_appserver][:version]}"
-default[:nginx_appserver][:passenger_root] = "#{default[:nginx_appserver][:ruby_path]}/lib/ruby/gems/#{`ls -1 #{default[:nginx_appserver][:ruby_path]}/lib/ruby/gems/ | sort -n | head -n 1`.strip}/gems/passenger-#{default[:nginx_appserver][:version]}"
+default[:nginx_appserver][:passenger_root] = "#{`#{default[:nginx_appserver][:ruby_path]}/bin/gem environment gemdir`.strip}/gems/passenger-#{default[:nginx_appserver][:version]}"
 default[:nginx_appserver][:user]           = 'www-data'
 default[:nginx_appserver][:root_dir]       = '/opt/nginx'
 default[:nginx_appserver][:cache_dir]      = '/var/cache/nginx'
@@ -13,3 +13,4 @@ default[:nginx_appserver][:pid_file]       = '/var/run/nginx.pid'
 default[:nginx_appserver][:log_dir]        = '/var/log/nginx'
 default[:nginx_appserver][:error_log]      = '/var/log/nginx/error.log'
 default[:nginx_appserver][:port]           = '80'
+#{`gem environment gemdir`.strip}
