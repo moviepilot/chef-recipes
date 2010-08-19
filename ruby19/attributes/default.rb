9682f156fc54d741f1112178f806f0a99b2b754a
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-19 11:00:53 +0200

    changed ruby version to 1.9.2

--- ruby19/attributes/default.rb
+++ ruby19/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:ruby19] = {}
 default[:ruby19][:version_major] = '1.9.2'
-default[:ruby19][:version_minor] = 'rc2'
+default[:ruby19][:version_minor] = 'p0'
 default[:ruby19][:version] = "#{node[:ruby19][:version_major]}-#{node[:ruby19][:version_minor]}"
 default[:ruby19][:package_name] = "ruby-#{node[:ruby19][:version]}.tar.bz2"
 default[:ruby19][:package_url] = "http://www.mirrorservice.org/sites/ftp.ruby-lang.org/pub/ruby/#{node[:ruby19][:package_name]}"
