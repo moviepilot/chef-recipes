bdc35ed581ad950ce087ba9841e5731142655648
Daniel Bornkessel <github@bornkessel.com>
2010-08-23 17:30:39 +0200

    la la la

--- ruby19/recipes/install.rb
+++ ruby19/recipes/install.rb
@@ -43,7 +43,7 @@ execute "#{node[:ruby19][:link_dir]}/bin/gem update --system" do
 end
 
 execute "set environment vars" do
-  command "sed -i -e 's%\\(PATH=\\)%\\1#{node[:ruby19][:link_dir]}/lib/ruby/gems/#{node[:ruby19][:gems_version]}/bin/:#{node[:ruby19][:link_dir]}/bin/%g' -e 's/\"//g' /etc/environment"
+  command "sed -i -e 's%\\(PATH=\\)%\\1#{node[:ruby19][:link_dir]}/lib/ruby/gems/#{node[:ruby19][:gems_version]}/bin/:#{node[:ruby19][:link_dir]}/bin/:%g' -e 's/\"//g' /etc/environment"
 
 end
 
