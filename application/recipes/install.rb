984c4cbf4a2523cc1a610a71950bacba1555c2f8
Daniel Bornkessel <github@bornkessel.com>
2010-08-23 11:33:42 +0200

    append, not replace

--- application/recipes/install.rb
+++ application/recipes/install.rb
@@ -15,7 +15,7 @@ node[:deploy].each do |application, deploy|
 end
 
 execute "set environment vars" do
-  command "echo 'RAILS_ENV=production' > /etc/environment"
+  command "echo 'RAILS_ENV=production' >> /etc/environment"
 end
 
 include_recipe "application::deploy"
