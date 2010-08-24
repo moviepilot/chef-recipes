616db74a4d25a46d568f297fe90bd1cecd28a15a
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-24 17:36:42 +0200

    added monit nginx-balancer

--- nginx-balancer/attributes/default.rb
+++ nginx-balancer/attributes/default.rb
@@ -4,4 +4,6 @@ default[:nginx_balancer][:package_name] = "nginx-#{node[:nginx_balancer][:versio
 default[:nginx_balancer][:package_url] = "http://nginx.org/download/#{node[:nginx_balancer][:package_name]}"
 default[:nginx_balancer][:target_dir] = "/opt/nginx-#{node[:nginx_balancer][:version]}"
 default[:nginx_balancer][:link_dir] = "/opt/nginx"
+default[:nginx_balancer][:pid_file]       = '/var/run/nginx.pid'
+default[:nginx_balancer][:port]           = '80'
 
