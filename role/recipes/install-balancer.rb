802d82ba5324f31ee3ff8ad0a39a2008d2ae1b1f
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-24 16:01:18 +0200

    switched role balancer location

--- role/recipes/install-balancer.rb
+++ role/recipes/install-balancer.rb
@@ -1,9 +1,10 @@
+execute "echo 'balancer' > #{node[:role][:role_file]}" do
+  cwd "/etc"
+end
+
 include_recipe 'role::install'
 include_recipe 'nginx-balancer::install'
 include_recipe 'varnish::install'
 include_recipe 'pen::install'
 
-execute "echo 'balancer' > #{node[:role][:role_file]}" do
-  cwd "/etc"
-end
 
