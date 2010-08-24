71aee9b3a057fe63d5b194166922240d37e676d9
Daniel Bornkessel <github@bornkessel.com>
2010-08-24 11:03:03 +0200

    finish balancer configuration

--- application/recipes/configure-balancer.rb
+++ application/recipes/configure-balancer.rb
@@ -1,7 +1,8 @@
-node["scalarium"]["roles"][ node[:app_identifier] ]["instances"].each do |instance|
-  internal_ip = instance[ instance.keys.first ]["private_ip"]
+node["scalarium"]["roles"][ node[:app_identifier] ]["instances"].each_with_index do |instance, index|
+  internal_ip = [*instance].flatten[1]["private_ip"]  # this works in ruby 1.8.7 & 1.9.x (1.8.7 does Array-of-Hashes -> Array-of-Arrays on each)
 
   execute "configure balancer" do
-    command "echo #{internal_ip} >> /tmp/foo"
+    command "penctl localhost:8081 server #{index} address #{internal_ip} port 80"
   end
 end
+
