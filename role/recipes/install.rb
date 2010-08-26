0d6b5e6ab8722ee1bc35bbdf160d52f7dbda586a
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-26 12:38:00 +0200

    added bash profile to default install

--- role/recipes/install.rb
+++ role/recipes/install.rb
@@ -1,2 +1,16 @@
 include_recipe 'scalper::install'
 include_recipe 'ruby19::install'
+
+template "/root/.bashrc" do
+  source "bashrc.erb"
+  owner "root"
+  group "root"
+  mode  "0644"
+end
+
+template "/home/ubuntu/.bashrc" do
+  source "bashrc.erb"
+  owner "ubuntu"
+  group "ubuntu"
+  mode  "0644"
+end
