12078c2e96070233630de08fef5cc0945ca55294
Caspar Clemens Mierau <ccm@screenage.de>
2010-08-26 10:50:45 +0200

    typo typo typo fix

--- varnish/attributes/default.rb
+++ varnish/attributes/default.rb
@@ -3,5 +3,5 @@ default[:varnish][:version] = '2.1.2'
 default[:varnish][:package_name] = "varnish-#{node[:varnish][:version]}.tar.gz"
 default[:varnish][:memory_limit] = "256M"
 default[:varnish][:port] = 9312
-default[:varnish][:target_dir]     = "/opt/varnis-#{node[:varnis][:version]}"
-default[:varnish][:link_dir]       = "/opt/varnis"
+default[:varnish][:target_dir]     = "/opt/varnish-#{node[:varnish][:version]}"
+default[:varnish][:link_dir]       = "/opt/varnish"
