89cc1fc15139892ad025501a0b7e24542bcf685f
Benjamin Krause <bk@benjaminkrause.com>
2010-09-01 15:40:14 +0200

    updated resque recipes to use a project resque_worker.yml file for worker configuration

--- resque/attributes/default.rb
+++ resque/attributes/default.rb
@@ -1,8 +1,7 @@
 default[:resque] = {}
 default[:resque][:version] = '1.9.7'
-default[:resque][:queues] = '*'
-default[:resque][:workers] = 12
 default[:resque][:max_memory] = 300
 default[:resque][:web] = {}
 default[:resque][:web][:enabled] = true
 default[:resque][:resque_config] = "/etc/rails/resque.yml"
+default[:resque][:resque_worker_config] = "./current/config/resque_worker.yml"
--- resque/recipes/deploy.rb
+++ resque/recipes/deploy.rb
@@ -1,11 +1,18 @@
 node[:deploy].each do |application, deploy|
-  queues_slug = node[:resque][:queues] == "*" ? "" : "_#{node[:resque][:queues].split(",").join("_")}"
-  template "/etc/monit/conf.d/resque_#{application}#{queues_slug}.monitrc" do
-    source "resque.monit.erb"
-    variables :queues_slug => queues_slug,
-              :deploy => deploy,
-              :application => application
-  end
+  redis_worker_config = YAML.load_file "#{deploy['deploy_to']}/#{node[:resque][:resque_worker_config]}" rescue next
+  execute "rm /etc/monit/conf.d/resque_#{application}*.monitrc"
+  
+  redis_worker_config['queues'].each do |queue|
+    queue_name = queue['queue_name']
 
+    template "/etc/monit/conf.d/resque_#{application}_#{queue_name}.monitrc" do
+      source "resque.monit.erb"
+      variables :queue_name    => queue_name,
+                :worker_amount => queue['worker'],
+                :deploy        => deploy,
+                :application   => application
+    end
+  end
+  
   execute "monit reload && sleep 60 && monit restart -g resque_workers_#{application}"
 end
