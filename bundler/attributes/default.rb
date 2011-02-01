870c4ec4adb1bcc949ec8df23d39b2b614d58028
Caspar Clemens Mierau <ccm@screenage.de>
2011-02-01 17:26:16 +0100

    switching to hard coded gem version

--- bundler/attributes/default.rb
+++ bundler/attributes/default.rb
@@ -1,3 +1,5 @@
 default[:bundler] = {}
-default[:bundler][:version] = '1.0.0.rc.1'
+default[:bundler][:version] = '1.0.7'
+default[:rubygems][:version] = '1.3.7'
 default[:bundler][:gem_binary] = '/opt/ruby/bin/gem'
+default[:bundler][:ruby_binary] = '/opt/ruby/bin/ruby'
--- bundler/recipes/install.rb
+++ bundler/recipes/install.rb
@@ -1,10 +1,36 @@
-execute "gem update --system" do
-  command "gem update --system"
-  action :run
+remote_file "/tmp/rubygems-#{node[:bundler][:gems_version]}.tgz" do
+  source "http://production.cf.rubygems.org/rubygems/rubygems-#{node[:bundler][:gems_version]}.tgz"
+  not_if do
+    File.exists?("/tmp/rubygems-#{node[:bundler][:gems_version]}.tgz")
+  end
 end
+
+execute "tar xvfz rubygems-#{node[:bundler][:gems_version]}.tgz" do
+  cwd "/tmp"
+  umask 022
+  not_if do
+    File.exists?("/tmp/rubygems-#{node[:bundler][:gems_version]}")
+  end
+end
+
+execute "Updating Rubygems to #{node[:bundler][:gems_version]}" do
+  command "#{node[:bundler][:ruby_binary]} setup.rb"
+  cwd "/tmp/rubygems-#{node[:bundler][:gems_version]}"
+  umask 022
+  not_if do
+    File.exists?(node[:bundler][:gem_binary]) && `#{node[:bundler][:gem_binary]} -v`.strip == node[:bundler][:gems_version]
+  end
+end
+
+
 
 gem_package 'bundler' do
   version node[:bundler][:version]
   gem_binary "#{node[:bundler][:gem_binary]}"
   retries 2
 end
+
+
+
+
+
