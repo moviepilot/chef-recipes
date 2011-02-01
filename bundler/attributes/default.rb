bceaae2be5faea1646fb45ab33e7e483ff90c5f8
Caspar Clemens Mierau <ccm@screenage.de>
2011-02-01 17:28:24 +0100

    fixing last commit

--- bundler/attributes/default.rb
+++ bundler/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:bundler] = {}
 default[:bundler][:version] = '1.0.7'
-default[:rubygems][:version] = '1.3.7'
+default[:bundler][:gems_version] = '1.3.7'
 default[:bundler][:gem_binary] = '/opt/ruby/bin/gem'
 default[:bundler][:ruby_binary] = '/opt/ruby/bin/ruby'
